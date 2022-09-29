f0a9f94a11d6504541e5ffc549ccc31463f5cf49
Shion-Serizawa <61482303+Shion-Serizawa@users.noreply.github.com>
2022-09-29 23:39:03 +0900

    6-2 最初のコード

--- src/App.js
+++ src/App.js
@@ -1,25 +1,75 @@
 import logo from './logo.svg';
 import './App.css';
+import PropTypes from 'prop-types'
+import {Component} from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+// function App() {
+//   return (
+//     <div className="App">
+
+//     </div>
+//   );
+// }
+
+class App extends Component  {
+  constructor(props) {
+    super(props)
+    this.state = { human: null, computer: null }
+  }
+
+  pon(human_hand) {
+    const computer_hand = Math.floor(Math.random() * 3)
+    this.setState({ human: human_hand, computer: computer_hand })
+  }
+
+  judge(){
+    if(this.state.human == null){
+      return null
+    }else{
+      return (this.state.computer - this.state.human + 3) % 3
+    }
+  }
+
+  render(){
+    return(
+      <div>
+        <h1>じゃんけん　ポン!</h1>
+        <JyankenBox actionPon={(te) => this.pon(te)} />
+        <ScoreBox human={this.state.human} computer={this.state.computer} judgement={this.judge()} />
+      </div>
+    )
+  }
+}
+
+const JyankenBox = (props) => {
+  return(
+    <div>
+      <button onClick={()=> props.actionPon(0)}>グー</button>
+      <button onClick={()=> props.actionPon(1)}>チョキ</button>
+      <button onClick={()=> props.actionPon(2)}>パー</button>
     </div>
-  );
+  )
+}
+JyankenBox.propTypes = {
+  actionPon: PropTypes.func
+}
+
+const ScoreBox =(props) => {
+  const teString = ["グー", "チョキ", "パー"]
+  const judgementString = ["引き分け","勝ち", "負け"]
+  return(
+    <table>
+      <tbody>
+        <tr><th>あなた</th><td>{teString[props.human]}</td></tr>
+        <tr><th>Computer</th><td>{teString[props.computer]}</td></tr>
+        <tr><th>勝敗</th><td>{judgementString[props.judgement]}</td></tr>
+      </tbody>
+    </table>
+  )
+}
+
+ScoreBox.propTypes = {
+  human: PropTypes.number
 }
 
 export default App;
